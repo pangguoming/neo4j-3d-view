bead454493ae71bcf63a114a2ea4dd67a7e004c8
pangguoming <pangguoming@yeah.net>
2019-12-26 18:04:56 +0800

    add flycontrol for 3d sence

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 import * as NEO4J from 'neo4j-driver';
+import {FlyControls} from './FlyControls.js';
 import './App.css';
 
+const clock = new THREE.Clock();
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = { clock:{} };
     }
 
     initNeo4j(){
@@ -15,7 +17,6 @@ class App extends Component {
         NEO4J.auth.basic('neo4j', 'pgmopen')
       );
       var session = driver.session({ defaultAccessMode: NEO4J.session.READ });
-      // Run a Cypher statement, reading the result in a streaming manner as records arrive:
       session
       .run('MERGE (alice:Person {name : $nameParam}) RETURN alice.name AS name', {
         nameParam: 'Alice'
@@ -35,7 +36,6 @@ class App extends Component {
           console.log(error)
         }
       })
-      debugger;
     }
  
     initThree(){
@@ -83,20 +83,37 @@ class App extends Component {
             var mesh = new THREE.Mesh(geometry, material);
             mesh.position.set(0, 0, 0);
             scene.add(mesh);
-        }        
+        }   
+        
+        /* 控制器 */
+        var controls;
+        function initControls() {
+          /* 飞行控件 */
+          controls = new FlyControls(camera, renderer.domElement);
+          /* 属性参数默认 */
+          controls.rollSpeed = Math.PI / 24; // 翻滚速度
+          controls.autoForward = true; //自动向前移动
+          controls.dragToLook = false;
+          controls.movementSpeed = 25; //移动速度
+      }
+
  
         function threeStart() {
             init();
             initCamera();
             initScene();
             initLight();
+            initControls();
             initObject();
             animation();
  
         }
+
+        
         function animation() {         
             renderer.render(scene, camera);
             requestAnimationFrame(animation);
+            controls.update(clock.getDelta());
         }
     }
  
@@ -111,47 +128,47 @@ class App extends Component {
     }
     render() {
         return (
-          <div>
-            <div id='cypher-frame'>
-              <h1>Cypher Runner for New Remoting</h1>
-              <div >
-                bolt url:<input type='text' id='bolturl' value='bolt://localhost:7687' />
-                user name: <input type='text' id='username' value='neo4j' />
-                password: <input type='text' id='password'  value='neo4j' />
-                <input id='connectserver' type='button' value='Connect' />
-              </div>      
-              <table width='100%' border='0' cellspacing='0' cellpadding='0'>
-                <tr>
-                  <td id='console'>
-                    <form>
-                      <table>
-                        <tr>
-                          <td class='td1' rowspan='2'><textarea id='statement'>MATCH (n) RETURN n LIMIT 10</textarea></td>
-                          <td class='td2'>
-                            <table id='parameters'><tbody>
-                              <tr>
-                                <td><input type='text' id='key' placeholder='(parameter)'/></td>
-                                <td><input type='text' id='value' placeholder='(value)'/></td>
-                                <td width='20'><input id='addParameter' type='button' value='+'/></td>
-                              </tr>
-                            </tbody></table>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td class='.td3' > 
-                          <input id='runButton' type='button' value='RUN' onfocus='this.blur()' />
-                          </td>
-                        </tr>
-                      </table>
-                    </form>
-                    <div id='results'></div>
-                  </td>
-                </tr>
-              </table>          
-            </div>
+          // <div>
+          //   <div id='cypher-frame'>
+          //     <h1>Cypher Runner for New Remoting</h1>
+          //     <div >
+          //       bolt url:<input type='text' id='bolturl' value='bolt://localhost:7687' />
+          //       user name: <input type='text' id='username' value='neo4j' />
+          //       password: <input type='text' id='password'  value='neo4j' />
+          //       <input id='connectserver' type='button' value='Connect' />
+          //     </div>      
+          //     <table width='100%' border='0' cellspacing='0' cellpadding='0'>
+          //       <tr>
+          //         <td id='console'>
+          //           <form>
+          //             <table>
+          //               <tr>
+          //                 <td class='td1' rowspan='2'><textarea id='statement'>MATCH (n) RETURN n LIMIT 10</textarea></td>
+          //                 <td class='td2'>
+          //                   <table id='parameters'><tbody>
+          //                     <tr>
+          //                       <td><input type='text' id='key' placeholder='(parameter)'/></td>
+          //                       <td><input type='text' id='value' placeholder='(value)'/></td>
+          //                       <td width='20'><input id='addParameter' type='button' value='+'/></td>
+          //                     </tr>
+          //                   </tbody></table>
+          //                 </td>
+          //               </tr>
+          //               <tr>
+          //                 <td class='.td3' > 
+          //                 <input id='runButton' type='button' value='RUN' onfocus='this.blur()' />
+          //                 </td>
+          //               </tr>
+          //             </table>
+          //           </form>
+          //           <div id='results'></div>
+          //         </td>
+          //       </tr>
+          //     </table>          
+          //   </div>
 
             <div id='canvas-frame'>  </div>
-          </div>
+          //</div>
         );
     }
 }
